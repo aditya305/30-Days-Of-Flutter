654b9fe765a0227987c820afebb76b791e5602a3
Aditya Sutar <41361167+aditya305@users.noreply.github.com>
2019-12-20 13:42:05 +0530

    New App Update
    App

---
+++ lib/bootscreen.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+
+class BootScreen extends StatefulWidget {
+  @override
+  _BootScreenState createState() => _BootScreenState();
+}
+
+class _BootScreenState extends State<BootScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 7);
+    return new Timer(_duration, navigationPage);
+  }
+  
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/LoginScreen');
+  }
+
+  @override
+  void initState() { 
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white12,
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset(
+              "assets/logo/logo.png",
+              scale: 5,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 
 import 'package:flutter/material.dart';
-import 'package:myapp/Day1/MyApp.dart';
+import 'package:flutter/services.dart';
+import 'package:myapp/Login.dart';
+import 'package:myapp/bootscreen.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DayByDay',
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData.dark(),
+      home: BootScreen(),
+
+      routes: <String,WidgetBuilder>{
+        '/BootScreen' : (BuildContext context) => new BootScreen(),
+        '/LoginScreen' : (BuildContext context) => new Login(),
+      },
+    );
+  }
+}
