9d1589b4702227e230c75071aefbf1d6f3421546
Aditya Sutar <41361167+aditya305@users.noreply.github.com>
2019-12-26 03:14:20 +0530

    Final Log in screen
    FInally found best log in screen for my app :)

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Login extends StatelessWidget {
   const Login({Key key}) : super(key: key);
@@ -7,25 +10,47 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          RichText( text: TextSpan(text: '\nWelcome to', children: [
-            TextSpan(
-              text: 'DayByDay'
-            )
-          ]),),
-          SizedBox(height: 40.0),
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Email Address', border: OutlineInputBorder()
+          SizedBox(height: 80),
+          Text('Welcome to\n   Day2Day', style: TextStyle(color: Colors.black, fontSize: 40),),
+          SizedBox(height: 70),
+          Container(
+            height: 50, width: 330,
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true, fillColor: Colors.grey[350],  
+                hintText: 'Email address', hintStyle: TextStyle(color: Colors.grey[700]), 
+              ), 
             ),
           ),
-          SizedBox(height: 60.0,),
-          RaisedButton(onPressed: () {}, 
-          child: Text('Signin'),
+          SizedBox(height: 20.0),
+          ButtonTheme(
+            minWidth: 330, height: 48.7,
+                      child: RaisedButton(
+              child: Text('Sign in', style: TextStyle(fontSize: 25), ), onPressed: () {}, 
+            ),
+          ),
+          SizedBox(height: 60),
+          Container(
+            width: 25.0,height: 25.0,
+            child: RaisedButton(
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset('assets/Signin_logo/google.svg'), Padding(
+                    padding: const EdgeInsets.symmetric( horizontal: 100,  ), 
+                    child: Text('Google', style: TextStyle(fontSize: 25, color: Colors.black), ),
+                  ),
+                ],
+              ), 
+              onPressed: () {}, color: Colors.white, 
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5), side: BorderSide(color: Colors.black),)
+            ),
           ),
         ],
+        ),
       ),
     );
   }
